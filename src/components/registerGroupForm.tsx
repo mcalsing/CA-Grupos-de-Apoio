d4bb6970692d5d10877766e349c1038f64ac44c0
mcalsing <marcelocalsing@gmail.com>
2025-04-09 11:06:17 -0300

    feat: adicionado os campos responsável e numero do contrado ao formulário

--- src/components/registerGroupForm.tsx
+++ src/components/registerGroupForm.tsx
@@ -1,34 +1,70 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { IAddress } from '../interfaces/IAddress'
 import axios from 'axios'
+import { IUserData } from '../interfaces/IUserData';
 
 function registerGroupForm() {
-  const [fullAddress, setFullAddress] = useState<IAddress | null>(null);
-  const [zipCode, setZipCode] = useState('')
+  const [dataGroup, setDataGroup] = useState<IAddress | null>(null);
+  const [cepInput, setCepInput] = useState('')
+  const [userData, setUserData] = useState<IUserData>({
+    responsible: '',
+    contractNumber: ''
+  });
 
-  const getCep = async (zipCode:string): Promise<void> => {
-    if (zipCode?.length == 8) {
-      const { data } = await axios.get<IAddress>(`https://viacep.com.br/ws/${zipCode}/json/`)
-      console.log(data)
-      setFullAddress(data)
-    }
+  const getCep = async (cep:string): Promise<void> => {
+    const { data } = await axios.get<IAddress>(`https://viacep.com.br/ws/${cep}/json/`)
+    setDataGroup(data)
   }
 
-  useEffect(() => {
-    getCep(zipCode)
-  }, [zipCode])
+  const handleCepChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const cleanedValue = e.target.value.replace(/\D/g, '');
+    setCepInput(cleanedValue);
+    
+    if (cleanedValue.length === 8) {
+      getCep(cleanedValue);
+    }
+  };
+
+  const handleUserDataChange = (e: React.ChangeEvent<HTMLInputElement>) => { 
+    const { name, value } = e.target;
+
+    setUserData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  //console.log(userData) verificar pq userdata "muda" quando o cep é digitado
 
   return (
     <form className="flex justify-center">
       <div className="mt-16 flex gap-4 flex-wrap md:w-[536px] w-[416px]">
         <div className="flex flex-col md:w-[259px] w-[200px]">
+          <label className="font-medium text-sm text-b600">Responsável</label>
+          <input 
+            className="py-[10px] px-[15px] border-1 border-b100 rounded-[4px]" 
+            type="text"
+            value={userData.responsible}
+            name="responsible"
+            onChange={handleUserDataChange}
+          />
+        </div>
+        <div className="flex flex-col md:w-[259px] w-[200px]">
+          <label className="font-medium text-sm text-b600">Número do contrato</label>
+          <input 
+            className="py-[10px] px-[15px] border-1 border-b100 rounded-[4px]" 
+            type="text"
+            value={userData.contractNumber}
+            name="contractNumber"
+            onChange={handleUserDataChange}
+          />
+        </div>
+        <div className="flex flex-col md:w-[259px] w-[200px]">
           <label className="font-medium text-sm text-b600">CEP</label>
           <input 
             className="py-[10px] px-[15px] border-1 border-b100 rounded-[4px]" 
             type="text"
-            maxLength="8"
-            value={zipCode}
-            onChange={e => setZipCode(e.target.value.replace(/\D/g, ''))}
+            maxLength={8}
+            value={cepInput}
+            onChange={handleCepChange}
+            name="cep"
           />
         </div>
         <div className="flex flex-col md:w-[259px] w-[200px]">
@@ -36,7 +72,8 @@ function registerGroupForm() {
           <input 
             className="py-[10px] px-[15px] border-1 border-b100 rounded-[4px]" 
             type="text"
-            value={fullAddress?.estado}
+            value={dataGroup?.estado}
+            name="state"
           />
         </div>
         <div className="flex flex-col md:w-[259px] w-[200px]">
@@ -44,7 +81,8 @@ function registerGroupForm() {
           <input 
             className="py-[10px] px-[15px] border-1 border-b100 rounded-[4px]" 
             type="text"
-            value={fullAddress?.localidade}
+            value={dataGroup?.localidade}
+            name="city"
           />
         </div>
         <div className="flex flex-col md:w-[259px] w-[200px]">
@@ -52,7 +90,8 @@ function registerGroupForm() {
           <input 
             className="py-[10px] px-[15px] border-1 border-b100 rounded-[4px]"
             type="text"
-            value={fullAddress?.bairro}
+            value={dataGroup?.bairro}
+            name="district"
           />
         </div>
         <div className="flex flex-col w-full">
@@ -60,7 +99,8 @@ function registerGroupForm() {
           <input 
             className="py-[10px] px-[15px] border-1 border-b100 rounded-[4px]" 
             type="text"
-            value={fullAddress?.logradouro}
+            value={dataGroup?.logradouro}
+            name="street"
           />
         </div>
       </div>
---
+++ src/interfaces/IUserData.ts
@@ -0,0 +1,4 @@
+export interface IUserData {
+  responsible: string;
+  contractNumber: string;
+}
